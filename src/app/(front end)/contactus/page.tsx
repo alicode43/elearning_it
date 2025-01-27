a9eb4757c1f3e8308fbd12451d49ead1dbd0b671
Ali <126307892+alicode43@users.noreply.github.com>
2025-01-28 01:04:55 +0530

    Change in footer

--- src/app/(front end)/contactus/page.tsx
+++ src/app/(front end)/contactus/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import { Footer } from '@/components/Footer'
+
 
 function page() {
   return (
